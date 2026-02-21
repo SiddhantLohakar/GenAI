a3945d185018f744bb4af3bdbc73e9c950d87f8d
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2026-02-21 21:41:28 +0530

    Done with indexing phase

--- RAG_PROJECT/indexing.js
+++ RAG_PROJECT/indexing.js
@@ -1,6 +1,15 @@
-import  dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
 dotenv.config();
+
+
+
+
+async function indexing(){
 
 
 const PDF_PATH = './NodeJS.pdf';
@@ -8,5 +17,37 @@ const pdfLoader = new PDFLoader(PDF_PATH);
 const rawDocs = await pdfLoader.load();
 
 
-console.log(JSON.stringify(rawDocs, null, 2));
-console.log(rawDocs.length)
+
+
+// Splittint the  document into chunks
+
+const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
+const chunkedDocs = await splitter.splitDocuments(rawDocs);
+
+
+// Initialized the embedding model
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: 'text-embedding-004',
+  });
+
+
+// Initialized the pinecone client
+const pinecone = new Pinecone();
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+
+// Storing the vector embeddings in the vector db
+try{
+  await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+    pineconeIndex,
+    maxConcurrency: 5,
+  });
+}
+catch(err){
+  console.log(err);
+}
+
+}
+
+indexing();
